9a31de68b35e29106738672f789a07c3f79bfc3a
Reobotte <reobotte@hotmail.com>
2022-04-16 02:13:58 -0400

    Adicionar arquivos de projeto.

---
+++ src/Empresa.ProdutosApi/Controllers/ProdutosController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa.ProdutosApi.Controllers
+{
+    // ToDo: Implementar gerenciamento para controle de versão
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Get() =>
+            Ok();
+
+        [HttpGet("categoria")]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> GetProdutosCategoria([FromQuery] Guid? categoriaId) =>
+            Ok();
+
+        [HttpGet("categoria/{produtoId}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetProdutoCategoria(Guid produtoId) =>
+            Ok();
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Get(Guid id) =>
+            Ok();
+
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Post() =>
+            Ok();
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Put(Guid id) =>
+            Ok();
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        public async Task<IActionResult> Delete(Guid id) =>
+            Ok();
+
+        #region Private Methods
+
+        private string GetLink(Guid id) =>
+            new StringBuilder(Url.Link("DefaultApi", new { id }))
+                .Replace("%2F", "/")
+                .ToString();
+
+        private IActionResult Resultado(IEnumerable<string> result)
+        {
+            AddModelError(result.ToList());
+            return BadRequest(ModelState);
+        }
+
+        private void AddModelError(List<string> erros) =>
+            erros.ToList()
+                .ForEach(erro => ModelState.AddModelError(string.Empty, erro));
+
+        #endregion
+    }
+}
---
+++ src/Empresa.ProdutosApi/Startup.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace Empresa.ProdutosApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                // Configura saída do JSon
+                .AddNewtonsoftJson(opt =>
+                {
+                    opt.SerializerSettings.Converters.Add(new StringEnumConverter());
+                    opt.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+                    opt.SerializerSettings.DateFormatString = "dd/MM/yyyy";
+                });
+
+            // Versioning API
+            services.AddApiVersioning(opt => {
+                opt.ReportApiVersions = true;
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 0);
+            });
+
+            #region Swegger
+
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Produtos API",
+                    Description = "API com arquitetura em DDD",
+                    // URL para os termos de serviço da API    
+                    // TermsOfService = new Uri("http://"), 
+                    // Informações do Contato da API
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Natael Corrêa",
+                        Email = "reobotte@hotmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/natael-corr%C3%AAa-32138417b/")
+                    },
+                    // Informações da Licença para API
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licença de uso da API",
+                        Url = new Uri("https://www.shutterstock.com/pt/license")
+                    }
+                });
+            });
+
+            #endregion Swegger
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            #region Swegger
+
+            app.UseSwagger();
+            app.UseSwaggerUI(opt =>
+            {
+                opt.RoutePrefix = string.Empty;
+                opt.SwaggerEndpoint("swagger/v1/swagger.json", "Produtos API");
+            });
+
+            #endregion
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("DefaultApi", "api/{controller=vlues}/v1/{id?}");
+            });
+        }
+    }
+}
